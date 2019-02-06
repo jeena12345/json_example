40443d9bc4d8d2dc7d3d54a892ebea41691edc36
jeena12345 <46891864+jeena12345@users.noreply.github.com>
2019-02-06 16:08:13 +0530

    Merge branch 'master' of https://github.com/jeena12345/json_example
    # Conflicts:
    #	app/src/main/java/com/example/inet/json_example/MainActivity.java

--- app/src/main/java/com/example/inet/json_example/MainActivity.java
+++ app/src/main/java/com/example/inet/json_example/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.inet.json_example;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         isInternetOn();
+        batteryLevel();
         uname = findViewById(R.id.uname);
         pwd = findViewById(R.id.pwd);
         login = findViewById(R.id.login);
@@ -88,6 +93,28 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return false;
+    }
+    private void batteryLevel() {
+        BroadcastReceiver batteryLevelReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                context.unregisterReceiver(this);
+                int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                int level = -1;
+                if (rawlevel >= 0 && scale > 0) {
+                    level = (rawlevel * 100) / scale;
+                }
+
+                if (level==100)
+                {
+                    moveTaskToBack(true);
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                }
+            }
+        };
+        IntentFilter batteryLevelFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryLevelReceiver, batteryLevelFilter);
     }
 }
 

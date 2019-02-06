ea8f21590abc99071d40cdce0c7d3d543ff518e4
jeena12345 <46891864+jeena12345@users.noreply.github.com>
2019-02-06 15:53:22 +0530

    Merge branch 'master' of https://github.com/jeena12345/json_example
    # Conflicts:
    #	app/src/main/java/com/example/inet/json_example/MainActivity.java

--- app/src/main/java/com/example/inet/json_example/MainActivity.java
+++ app/src/main/java/com/example/inet/json_example/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.inet.json_example;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,29 +19,46 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
-EditText uname;
-EditText pwd;
-Button login;
+    EditText uname;
+    EditText pwd;
+    Button login;
+    private Boolean exit = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        uname=findViewById(R.id.uname);
-        pwd=findViewById(R.id.pwd);
-        login=findViewById(R.id.login);
-login.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        if ((uname.getText().toString().equals("jasim")) && (pwd.getText().toString().equals("12345678")))
-
-        {
-            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
-        }
-    }});
+        uname = findViewById(R.id.uname);
+        pwd = findViewById(R.id.pwd);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((uname.getText().toString().equals("jasim")) && (pwd.getText().toString().equals("12345678"))) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish(); // finish activity
+        } else {
+            Toast.makeText(this, "Press Back again to Exit.",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 3 * 1000);
+
+        }
+    }
 }
-//heelow

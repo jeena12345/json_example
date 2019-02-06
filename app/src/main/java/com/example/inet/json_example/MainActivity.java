4f6721f59e7157dfd66e810ec7978c4d31fe8d3b
jeena12345 <jeenav1995@gmail.com>
2019-02-06 14:58:27 +0530

    Merge branch 'master' of https://github.com/jeena12345/json_example
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/inet/json_example/MainActivity.java
+++ app/src/main/java/com/example/inet/json_example/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.inet.json_example;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,12 +18,23 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
+EditText uname;
+EditText pwd;
+Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uname=findViewById(R.id.uname);
+        pwd=findViewById(R.id.pwd);
+        login=findViewById(R.id.login);
+login.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        if (uname.getText().toString().equals()"jasim");
+        Intent intent=new Intent(this,);
+    }
+});
 
     }
 

07bdaf6f8c55ced4fe2fb7e124680a033a20b0e5
jeena12345 <46891864+jeena12345@users.noreply.github.com>
2019-02-06 14:14:22 +0530

    ui deleted

--- app/src/main/java/com/example/inet/json_example/MainActivity.java
+++ app/src/main/java/com/example/inet/json_example/MainActivity.java
@@ -16,45 +16,13 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseReference rootRef,demoRef;
 
-    EditText ed1;
-    Button b1,b2;
-    TextView txt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ed1=findViewById(R.id.name);
-        b2=findViewById(R.id.fetch);
-        txt1=findViewById(R.id.txt2);
-        b1=findViewById(R.id.b1);
-        rootRef = FirebaseDatabase.getInstance().getReference();
-        demoRef = rootRef.child("jeena1").child("demo");
-        b1.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        demoRef.setValue(ed1.getText().toString());
 
-    }
-});
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                demoRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        String valu = dataSnapshot.getValue(String.class);
-                        txt1.setText(valu);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
-            }
-        });
     }
 
 }
